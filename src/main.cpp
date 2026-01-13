b8790ca3526726e39c3eae1740afebebad1109cc
enochul <enochuli@outlook.com>
2026-01-13 13:43:00 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+/*Use and edit this file to build and upload
+to the board while we are testing things out*/
+
+#define LDR_PIN 35 // LDR
+
+// Tune these by experiment
+const int dayThreshold = 1800; // above this = "daylight"
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int lightLevel = analogRead(LDR_PIN);
+
+  bool isDayLight = lightLevel > dayThreshold;
+
+  // What to show on screen to the user
+  if (isDayLight) {
+    Serial.println("Daylight");
+  } else {
+    Serial.println("No daylight");
+  }
+
+  Serial.print("Raw ADC: ");
+  Serial.println(lightLevel);
+
+  // Optional debugging info while tuning brightness settings
+  Serial.print("Raw ADC: ");
+  Serial.println(lightLevel);
+
+  Serial.println("-----");
+  delay(700);
+}
