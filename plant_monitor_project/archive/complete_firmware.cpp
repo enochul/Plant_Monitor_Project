1eab40ad22a8e8bdc07074d89dfa2063bb719aac
Dawson Osterhout <dawsonosterhout@icloud.com>
2026-01-26 13:01:28 -0700

    Add files via upload

---
+++ plant_monitor_project/archive/complete_firmware.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+/*This file will be the project's completed code, while the
+main.cpp can be used to run tests and play around for now*/
+
+#define LDR_PIN 35 // LDR
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+}
---
+++ plant_monitor_project/archive/daylight_sensor_test/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define LIGHT_PIN 35 // LDR
+
+// Tune these by experiment
+const int dayThreshold = 1800; // above this = "daylight"
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int lightLevel = analogRead(LIGHT_PIN);
+
+  bool isDayLight = lightLevel > dayThreshold;
+
+  // What to show on screen to the user
+  if (isDayLight) {
+    Serial.println("Daylight");
+  } else {
+    Serial.println("No daylight");
+  }
+
+  Serial.print("Raw ADC: ");
+  Serial.println(lightLevel);
+
+  // Optional debugging info while tuning brightness settings
+  Serial.print("Raw ADC: ");
+  Serial.println(lightLevel);
+
+  Serial.println("-----");
+  delay(700);
+}
